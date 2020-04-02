cef7371ba751caf3e818e0004367a20320940498
mpowalisz <markpowalisz@gmail.com>
2020-04-02 19:11:17 -0400

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute("src");
+
+  if (mySrc === "assets/images/firefox.png") {
+    myImage.setAttribute("src", "assets/images/blue-firefox.png");
+  } else {
+    myImage.setAttribute("src", "assets/images/firefox.png");
+  }
+};
+
+let myButtun = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Welcome to the Mozilla Exercise, " + myName;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Welcome to the Mozilla Exercise, " + storedName;
+}
